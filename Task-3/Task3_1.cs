d05ef2f60492e0f3e945d9fe7829ebefaaeb0e15
Jayakanthan Subramani <79399389+jayakanthansubramani@users.noreply.github.com>
2022-09-20 15:58:59 +0530

    Add files via upload

---
+++ Task-3/Task3_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class clac1
+    {
+        public void add(int num1,int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        public void sub(int num1,int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+    }
+
+    class calc2:clac1
+    {
+        public void multiply(int n1,int n2)
+        {
+            Console.WriteLine(n1*n2);
+        }
+
+        public void divide(int n1,int n2)
+        {
+            double div = n1/n2;
+            Console.WriteLine("the divition is {0}"+div);
+        }
+    }
+    internal class Task3_1
+    {
+        public static void Main()
+        {
+            calc2 obj = new calc2();
+            obj.add(20,30);
+            obj.divide(60,7);
+        }
+    }
+}
---
+++ Task-3/Validation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    internal class Validation
+    {
+        public void  val(string emailid,string pass)
+        {
+            if(emailid == "jaisubramani23@gmail.com")
+            {
+                Console.WriteLine("The emailid enterd is valid");
+            }
+
+
+        }
+
+        public void val(int memid,int pin)
+        {
+            if(memid == 234 && pin == 1234)
+            {
+                Console.WriteLine("Entred Correct Pin and MembershipID");
+            }
+        }
+
+        public void val(long mobno,int pin)
+        {
+            if(mobno == 63636662677 && pin== 56787)
+            {
+                Console.WriteLine("The pin and MobilNo is  correct");
+            }
+
+        }
+        public static void Main()
+        {
+            //val(883883888838,56787);
+        }
+    }
+}
