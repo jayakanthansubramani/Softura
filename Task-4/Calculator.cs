09df2d1ce8c65c14fe6e76982ecfb4aae31b5189
Jayakanthan Subramani <79399389+jayakanthansubramani@users.noreply.github.com>
2022-09-20 16:04:32 +0530

    Add files via upload

---
+++ Task-4/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class calc
+    {
+        public abstract void add(int num1,int num2);
+        public abstract void sub(int num1,int num2);
+        public abstract void mul(int num1,int num2);
+        public abstract void div(int num1,int num2);
+    }
+    class Cal : calc
+    {
+        public override void add(int n1,int num2)
+        {
+            int add = n1 + num2;
+            Console.WriteLine("Addition "+add);
+        }
+
+        public override void sub(int num1,int num2)
+        {
+            int sub = num1 - num2;
+            Console.WriteLine("Subtraction "+sub);
+        }
+
+        public override void mul(int num1,int num2)
+        {
+            Console.WriteLine("Multiplication ",num1 * num2);
+        }
+
+        public override void div(int num1,int num2)
+        {
+            double val = num1 / num2;
+            Console.WriteLine("Divition{0:E} ",+val);
+        }
+    }
+    internal class Calculator
+    {
+        public static void Main()
+        {
+            Cal calc = new Cal();
+            calc.add(23, 45);
+            calc.sub(277, 36);
+            calc.mul(6, 7);
+            calc.div(60, 5);
+        }
+    }
+}
---
+++ Task-4/MemberShip.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Packages
+    {
+        public abstract void Membership();
+    }
+
+    class Silver:Packages
+    {
+        public override void Membership()
+        {
+            Console.WriteLine("3 days accomodation at Resort");
+        }
+    }
+
+    class Gold : Packages
+    {
+        public override void Membership()
+        {
+            Console.WriteLine("5 days accomodation at Resort & 2Dinners on the house");
+
+        }
+    }
+
+    class Platinum : Packages
+    {
+        public override void Membership()
+        {
+            Console.WriteLine("7 days accomodation at Resort & 5 Dinners on the house");
+
+        }
+    }
+    internal class MemberShip
+    {
+        public static void Main()
+        {
+            Silver silver = new Silver();
+            silver.Membership();
+            Gold gold = new Gold();
+            gold.Membership();
+            Platinum platinum = new Platinum();
+            platinum.Membership();
+        }
+
+    }
+}
