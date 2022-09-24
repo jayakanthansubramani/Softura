ad9db0263bcd6d66bdb68f3408a5305a576bf232
Jayakanthan Subramani <79399389+jayakanthansubramani@users.noreply.github.com>
2022-09-24 16:16:57 +0530

    Add files via upload

---
+++ Task-3/TaxCalculation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class calcvat
+    {
+        public void ClaculateTax(double num1)
+        {
+            Console.WriteLine("You Need To Pay This Amount  As A tax:  {0}", num1 * 0.2);
+        }
+    }
+    class gst : calcvat
+    {
+        public void Calculategst(double num1)
+        {
+            Console.WriteLine("You Need To Pay this Amount As Gst:  {0}", num1 * 0.12);
+        }
+         
+      
+    }
+
+    internal class TaxCalculation
+    {
+        public static void Main()
+        {
+            public static void Main()
+        {
+            gst obj1 = new gst();
+            Console.WriteLine("Enter The Amount You nedd To Calculate Tax vat"  );
+            double rupees = Convert.ToInt32(Console.ReadLine());
+            obj1.ClaculateTax(rupees);
+            Console.WriteLine("Enter The Amount You nedd To Calculate Tax gst");
+            double rupees1 = Convert.ToInt32(Console.ReadLine());
+            obj1.Calculategst(rupees1);
+
+        }
+        }
+    }
+}
