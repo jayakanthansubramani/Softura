69b11d638a00dd75bf05948e1246b1637155de0c
Jayakanthan Subramani <79399389+jayakanthansubramani@users.noreply.github.com>
2022-09-24 06:56:35 +0530

    Add files via upload

---
+++ Task-2/Jagged.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Jagged
+    {
+        public static void Main()
+        {
+            string[][] patient = new string[3][];
+            patient[0] = new string[] { "Jai", "Dev", "Vikram" };
+            patient[1] = new string[] { "Yuvraj", "Vimal", "Vijay", "Diya" };
+            patient[2] = new string[] { "Nithya", "Riyaz", "Vix", "Raj", "Rajasekar", "Sekar" };
+            
+            for (int i = 0; i < patient.Length; i++)
+            {
+                for (int j = 0; j < patient[i].Length; j++)
+                {
+                    Console.WriteLine(patient[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ Task-2/Packages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Switch
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the package name: ");
+            Console.WriteLine("Choose A or B or C");
+            char package = Console.ReadLine()[0];
+            switch(package)
+            {
+                case 'A': Console.WriteLine(" *south special \n *Children's Club \n *Movies \n PRICE: 250");
+                break;
+                case 'B':Console.WriteLine(" *News \n *Sports \n *Movies \n *Regional-2 \n PRICE: 450");
+                break;
+                case 'C':Console.WriteLine(" *Discovery,History,National \n PRICE: 350");
+                break;
+                default: Console.WriteLine("Enter a Valid Input");
+                break;
+
+            }
+        }
+    }
+}
