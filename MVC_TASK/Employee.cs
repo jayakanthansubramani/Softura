a6fedd9e09ec6519b2d7b23b359ec57c57a89461
Jayakanthan Subramani <79399389+jayakanthansubramani@users.noreply.github.com>
2022-10-03 18:32:00 +0530

    Add files via upload

---
+++ MVC_TASK/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleApp.Models
+{ 
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Gender { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ MVC_TASK/EmployeeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleApp.Models;
+
+namespace SampleApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SingleEmpInfo()
+        {
+            List<Employee> emp = new()
+            {
+                new Employee()
+                {
+                    EmployeeID = 101,
+                    Firstname = "Jaya",
+                    Lastname = "Kanthan",
+                    Gender = "Male",
+                    City = "Vellore"
+                }
+            };
+            ViewBag.List = emp;
+            return View(emp);
+        }
+
+        public IActionResult AllEmpInfo()
+        {
+            List<Employee> emp = new()
+            {
+                new Employee()
+                {
+                    EmployeeID = 101,
+                    Firstname = "Jaya",
+                    Lastname = "Kanthan",
+                    Gender = "Male",
+                    City = "Vellore"
+                },
+                new Employee()
+                {
+                    EmployeeID = 102,
+                    Firstname = "Dev",
+                    Lastname = "Swamy",
+                    Gender = "Male",
+                    City = "Chennai"
+                },
+                new Employee()
+                {
+                    EmployeeID = 103,
+                    Firstname = "Dhanush",
+                    Lastname = "K",
+                    Gender = "Male",
+                    City = "Chennai"
+                },
+                new Employee()
+                {
+                    EmployeeID = 104,
+                    Firstname = "Krishna",
+                    Lastname = "M",
+                    Gender = "Male",
+                    City = "Chennai"
+                },
+                new Employee()
+                {
+                    EmployeeID = 105,
+                    Firstname = "Ajay",
+                    Lastname = "Rayen",
+                    Gender = "Male",
+                    City = "Chennai"
+                },
+            };
+            return View(emp);
+        }
+        public IActionResult EnterNewInfo()
+        {
+            return View();
+        }
+    }
+}
